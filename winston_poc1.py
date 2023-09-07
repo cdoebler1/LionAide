3b4d8eee1cd87fdacc6bf930369ebbeee64b9904
Charles Doebler <cdoebler1@gmail.com>
2023-09-07 12:52:48 -0400

    Create winston_poc1.py

---
+++ winston_poc1.py
@@ -0,0 +1,13 @@
+#pip install tkinter
+#pip install pywebview
+
+
+import tkinter as tk
+import webview
+  
+tk = tk.Tk()
+  
+tk.geometry("800x450")
+  
+webview.create_window('LionAide', 'http://73.175.148.240:7860')
+webview.start()
